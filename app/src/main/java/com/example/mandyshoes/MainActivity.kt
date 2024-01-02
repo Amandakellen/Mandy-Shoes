8dba9e8e8f5356207e619adacdba7cacbc356f08
Amanda Soares <amanda.soares@ifood.com.br>
2024-01-02 13:11:32 -0300

    Init Project

---
+++ app/src/main/java/com/example/mandyshoes/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mandyshoes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mandyshoes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

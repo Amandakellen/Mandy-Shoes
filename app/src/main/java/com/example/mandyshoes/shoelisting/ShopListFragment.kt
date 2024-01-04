ebbae5af9c50ef1750dac2dc610a22a28de51fa2
Amanda Soares <amanda.soares@ifood.com.br>
2024-01-04 13:53:33 -0300

    finish shop list

--- app/src/main/java/com/example/mandyshoes/shoelisting/ShopListFragment.kt
+++ app/src/main/java/com/example/mandyshoes/shoelisting/ShopListFragment.kt
@@ -1,21 +1,49 @@
 package com.example.mandyshoes.shoelisting
 
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
 import com.example.mandyshoes.R
+import com.example.mandyshoes.databinding.FragmentShopListBinding
 
 class ShopListFragment : Fragment() {
 
+    private lateinit var binding: FragmentShopListBinding
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shop_list, container, false)
+
+        binding = FragmentShopListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpViews()
+    }
+    private fun setUpViews(){
+        binding.greenTenis.setOnClickListener{
+            setArgument(R.mipmap.green_tenis_foreground)
+        }
 
+        binding.highHeels.setOnClickListener{
+            setArgument(R.drawable.high_heels)
+
+        }
+        binding.tenis.setOnClickListener{
+            setArgument(R.mipmap.tenis_foreground)
+        }
+    }
+
+    private fun setArgument(imageRes: Int){
+        val action = ShopListFragmentDirections.actionShopListFragmentToShopDetailFragment(imageRes)
+        findNavController().navigate(action)
+    }
 }
